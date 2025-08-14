71371c04da2eb917f4ed738c143489c6ce0e7264
Till <Till.Schoenenberg@outlook.com>
2025-08-14 22:26:20 +0200

    -added code a la mode bot -> bronze -PodRacer intermediate commit, gold ranked 900

---
+++ CodeALaMode/Program.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game
+{
+    public Player[] Players = new Player[2];
+    public Table Dishwasher;
+    public Table Window;
+    public Table Blueberry;
+    public Table IceCream;
+    public Table Strawberry;
+    public Table ChoppingBoard;
+    public List<Table> Tables = new List<Table>();
+    public List<Customer> Customers = new List<Customer>();
+}
+
+
+public enum Ingredient
+{
+    ICE_CREAM,
+    BLUEBERRIES,
+    STRAWBERRIES,
+    DISH,
+    CHOPPED_STRAWBERRIES,
+}
+public class Table
+{
+    public Position Position;
+    public bool HasFunction;
+    public Item Item;
+}
+
+public class  Customer
+{
+    public int Reward;
+    public Item Order;
+}
+
+public class Item
+{
+    public string Content;
+    public bool HasPlate;
+    public Item(string content)
+    {
+        Content = content;
+        HasPlate = Content.Contains(MainClass.Dish);
+    }
+
+    public List<Ingredient> Ingredients
+    {
+        get
+        {
+            if (Content == null || Content == "NONE") return new List<Ingredient>();
+
+            var ingredients = new List<Ingredient>();
+            var strings = Content.Split('-');
+
+            foreach(var s in strings)
+            {
+                Ingredient component;
+                if(Enum.TryParse(s, true, out component))
+                {
+                    ingredients.Add(component);
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Unknown ingredient: {s}");
+                }
+            }
+
+            return ingredients;
+        }
+    }
+}
+
+public class Player
+{
+    public Position Position;
+    public Item Item;
+    public Player(Position position, Item item)
+    {
+        Position = position;
+        Item = item;
+    }
+    public void Update(Position position, Item item)
+    {
+        Position = position;
+        Item = item;
+    }
+}
+
+public class Position
+{
+    public int X, Y;
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int Manhattan(Position p2) => Math.Abs(X - p2.X) + Math.Abs(Y - p2.Y);
+
+    public override string ToString()
+    {
+        return X + " " + Y;
+    }
+}
+
+
+
+public class MainClass
+{
+    public static bool Debug = true;
+    public const string Dish = "DISH";
+    public static string[,] Gameboard = new string[11, 7];
+
+    public static Game ReadGame()
+    {
+        var game = new Game();
+        game.Players[0] = new Player(null, null);
+        game.Players[1] = new Player(null, null);
+
+        for (int i = 0; i < 7; i++)
+        {
+            string kitchenLine = ReadLine();
+            for (var x = 0; x < kitchenLine.Length; x++)
+            {
+                Gameboard[x, i] = kitchenLine[x].ToString();
+
+                if (kitchenLine[x] == 'W') game.Window = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == 'D') game.Dishwasher = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == 'I') game.IceCream = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == 'B') game.Blueberry = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == 'S') game.Strawberry = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == 'C') game.ChoppingBoard = new Table { Position = new Position(x, i), HasFunction = true };
+                if (kitchenLine[x] == '#') game.Tables.Add(new Table { Position = new Position(x, i) });
+            }
+        }
+
+        return game;
+    }
+
+    private static void Move(Position p) => Console.WriteLine("MOVE " + p);
+
+    private static void Use(Position p)
+    {
+        Console.WriteLine($"USE {p}; {Gameboard[p.X, p.Y]}");
+    }
+
+    private static void Wait()
+    {
+        Console.WriteLine($"WAIT");
+    }
+
+    private static string ReadLine()
+    {
+        var s = Console.ReadLine();
+        if (Debug)
+            Console.Error.WriteLine($"INPUT: {s}");
+        return s;
+    }
+
+
+    static void Main()
+    {
+        string[] inputs;
+
+        List<Customer> customers = new List<Customer>();
+        // ALL CUSTOMERS INPUT: to ignore until Bronze
+        int numAllCustomers = int.Parse(ReadLine());
+        for (int i = 0; i < numAllCustomers; i++)
+        {
+            inputs = ReadLine().Split(' ');
+            string customerItem = inputs[0]; // the food the customer is waiting for
+            int customerAward = int.Parse(inputs[1]); // the number of points awarded for delivering the food
+            customers.Add(new Customer { Order = new Item(customerItem), Reward = customerAward });
+        }
+
+        // KITCHEN INPUT
+        var game = ReadGame();
+        game.Customers = customers;
+
+        while (true)
+        {
+            int turnsRemaining = int.Parse(ReadLine());
+
+            // PLAYERS INPUT
+            inputs = ReadLine().Split(' ');
+            game.Players[0].Update(new Position(int.Parse(inputs[0]), int.Parse(inputs[1])), new Item(inputs[2]));
+            inputs = ReadLine().Split(' ');
+            game.Players[1].Update(new Position(int.Parse(inputs[0]), int.Parse(inputs[1])), new Item(inputs[2]));
+
+            //Clean other tables
+            foreach (var t in game.Tables)
+            {
+                t.Item = null;
+            }
+            int numTablesWithItems = int.Parse(ReadLine()); // the number of tables in the kitchen that currently hold an item
+            for (int i = 0; i < numTablesWithItems; i++)
+            {
+                inputs = ReadLine().Split(' ');
+                var table = game.Tables.First(t => t.Position.X == int.Parse(inputs[0]) && t.Position.Y == int.Parse(inputs[1]));
+                table.Item = new Item(inputs[2]);
+            }
+
+            inputs = ReadLine().Split(' ');
+            string ovenContents = inputs[0]; // ignore until bronze league
+            int ovenTimer = int.Parse(inputs[1]);
+            int numCustomers = int.Parse(ReadLine()); // the number of customers currently waiting for food
+            Customer[] currentCustomers = new Customer[numCustomers];
+            for (int i = 0; i < numCustomers; i++)
+            {
+                inputs = ReadLine().Split(' ');
+                string customerItem = inputs[0];
+                int customerAward = int.Parse(inputs[1]);
+                currentCustomers[i] = new Customer { Order = new Item(customerItem), Reward = customerAward };
+            }
+
+            // GAME LOGIC
+            // fetch a dish, pick ice cream and drop the dish on an empty table
+
+
+            var myChef = game.Players[0];
+
+            Customer? customerToServe = GetCustomerToServe(currentCustomers, myChef.Item);
+            if (customerToServe == null)
+            {
+                Console.Error.WriteLine("No customer to serve found.");
+                Use(game.Dishwasher.Position);
+                continue;
+            }
+            List<Ingredient> componentsMissing = GetMissingIngredients(customerToServe.Order, myChef.Item);
+
+            Position? nextComponent = GetNearestIngredient(componentsMissing, myChef, game);
+            if(nextComponent == null)
+            {
+                Console.Error.WriteLine("ERROR: No next ingredient found.");
+                Wait();
+                continue;
+            }
+
+            Use(nextComponent);
+        }
+    }
+
+    private static Position? GetNearestIngredient(List<Ingredient> componentsMissing, Player myChef, Game game)
+    {
+        if(componentsMissing.Count == 0)
+        {
+            return game.Window.Position;
+        }
+
+        Console.Error.WriteLine($"Components missing: {string.Join(", ", componentsMissing)}");
+
+        Position myChefPosition = myChef.Position;
+        Position nearestPosition = null;
+        int minDistance = int.MaxValue;
+        int distance;
+        var ingredients = myChef.Item.Ingredients;
+        if (componentsMissing.Contains(Ingredient.CHOPPED_STRAWBERRIES))
+        {
+            if (!ingredients.Contains(Ingredient.STRAWBERRIES))
+                return game.Strawberry.Position;
+            if(!ingredients.Contains(Ingredient.CHOPPED_STRAWBERRIES))
+                return game.ChoppingBoard.Position;
+        }
+
+        if(ingredients.Contains(Ingredient.CHOPPED_STRAWBERRIES) && !ingredients.Contains(Ingredient.DISH))
+        {
+            return game.Dishwasher.Position;
+        }
+
+        foreach (var ingredient in componentsMissing)
+        {
+            switch(ingredient)
+            {
+                case Ingredient.ICE_CREAM:
+                    distance = myChefPosition.Manhattan(game.IceCream.Position);
+                    if (distance < minDistance)
+                    {
+                        nearestPosition = game.IceCream.Position;
+                        minDistance = distance;
+                    }
+                    break;
+                case Ingredient.BLUEBERRIES:
+                    distance = myChefPosition.Manhattan(game.Blueberry.Position);
+                    if (distance < minDistance)
+                    {
+                        nearestPosition = game.Blueberry.Position;
+                        minDistance = distance;
+                    }
+                    break;
+                case Ingredient.CHOPPED_STRAWBERRIES:
+                    if(myChef.Item.Ingredients.Contains(Ingredient.STRAWBERRIES))
+                    {
+                        distance = myChefPosition.Manhattan(game.ChoppingBoard.Position);
+                        if(distance < minDistance)
+                        {
+                            nearestPosition = game.ChoppingBoard.Position;
+                            minDistance = distance;
+                        }
+                    }
+                    else
+                    {
+                        distance = myChefPosition.Manhattan(game.Strawberry.Position);
+                        if (distance < minDistance)
+                        {
+                            nearestPosition = game.Strawberry.Position;
+                            minDistance = distance;
+                        }
+                    }
+                    break;
+                case Ingredient.DISH:
+                    distance = myChefPosition.Manhattan(game.Dishwasher.Position);
+                    if (distance < minDistance)
+                    {
+                        nearestPosition = game.Dishwasher.Position;
+                        minDistance = distance;
+                    }
+                    break;
+                default:
+                    Console.Error.WriteLine($"Unknown ingredient: {ingredient}");
+                    break;
+            }
+        }
+
+        return nearestPosition;
+    }
+
+    private static List<Ingredient> GetMissingIngredients(Item order, Item currentDish)
+    {
+        var needed = order.Ingredients;
+        var having = currentDish.Ingredients;
+
+        return needed
+            .Where(n => !having.Contains(n))
+            .ToList();
+    }
+
+
+
+    private static Customer? GetCustomerToServe(Customer[] currentCustomers, Item item)
+    {
+        // not started a dish yet
+        if (item == null || item.Content == "NONE")
+        {
+            return currentCustomers.Last(x => x.Reward == currentCustomers.Max(y => y.Reward));
+        }
+
+        //started a dish, filter matching customers
+        string[] componentsReady = item.Content.Split('-');
+        List<Customer> possibleCustomers = new List<Customer>();
+        foreach(var customer in currentCustomers)
+        {
+            string contents = customer.Order.Content;
+            if (!componentsReady.Any(c => contents.Contains(c)))
+            {
+                Console.Error.WriteLine($"No matching customer for {item.Content} in {contents}");
+                continue;
+            }
+
+            possibleCustomers.Add(customer);
+        }
+
+        return possibleCustomers
+            .LastOrDefault(x => x.Reward == possibleCustomers.Max(y => y.Reward));
+    }
+}
--- PodRacer/Program.cs
+++ PodRacer/Program.cs
@@ -1,115 +1,308 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Player
 {
-    // Persistent state across turns
-    static bool boostUsed = false;
-    static int prevX = int.MinValue, prevY = int.MinValue;
-    static int prevOppX = int.MinValue, prevOppY = int.MinValue;
+    // Shared race data
+    static int LAPS;
+    static int CP_COUNT;
+    static Point[] CP;
+
+    // Boost is shared between pods
+    static bool boostAvailable = true;
+
+    // Persistent trackers for our pods and opponents
+    static PodTracker[] my = { new PodTracker(), new PodTracker() };
+    static PodTracker[] opp = { new PodTracker(), new PodTracker() };
+
+    // Decide which of our pods is the runner (kept stable with hysteresis)
+    static int runnerId = -1;
 
     static void Main()
     {
+        Console.SetOut(new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+
+        // Init
+        LAPS = int.Parse(Console.ReadLine());
+        CP_COUNT = int.Parse(Console.ReadLine());
+        CP = new Point[CP_COUNT];
+        for (int i = 0; i < CP_COUNT; i++)
+        {
+            var p = Console.ReadLine().Split(' ');
+            CP[i] = new Point(int.Parse(p[0]), int.Parse(p[1]));
+        }
+
+        // Game loop
         while (true)
         {
-            // Read own pod state
-            string[] inputs = Console.ReadLine().Split(' ');
-            int x = int.Parse(inputs[0]);
-            int y = int.Parse(inputs[1]);
-            int nextCheckpointX = int.Parse(inputs[2]);
-            int nextCheckpointY = int.Parse(inputs[3]);
-            int nextCheckpointDist = int.Parse(inputs[4]);
-            int nextCheckpointAngle = int.Parse(inputs[5]);
-
-            // Read opponent (single pod)
-            inputs = Console.ReadLine().Split(' ');
-            int opponentX = int.Parse(inputs[0]);
-            int opponentY = int.Parse(inputs[1]);
-
-            // Compute velocity (approx from last frame)
-            int vx = 0, vy = 0;
-            if (prevX != int.MinValue)
+            // Read our pods
+            for (int i = 0; i < 2; i++)
             {
-                vx = x - prevX;
-                vy = y - prevY;
+                var s = Console.ReadLine().Split(' ');
+                my[i].Pos = new Point(int.Parse(s[0]), int.Parse(s[1]));
+                my[i].V = new Vec(int.Parse(s[2]), int.Parse(s[3]));
+                my[i].AngleAbs = int.Parse(s[4]);
+                my[i].NextId = int.Parse(s[5]);
+                my[i].UpdateProgress(CP_COUNT);
             }
-            double speed = Hypot(vx, vy);
-
-            // Opponent velocity and relative speed (for SHIELD)
-            int oppVx = 0, oppVy = 0;
-            if (prevOppX != int.MinValue)
+            // Read opponents
+            for (int i = 0; i < 2; i++)
             {
-                oppVx = opponentX - prevOppX;
-                oppVy = opponentY - prevOppY;
-            }
-            double relV = Hypot(vx - oppVx, vy - oppVy);
-            double oppDist = Hypot(opponentX - x, opponentY - y);
-
-            // Inertia-compensated aim (lead against our drift)
-            // Aim "inside" the turn by offsetting opposite to current velocity
-            // Factor tuned to ~3 frames of drift compensation
-            int leadFactor = 3;
-            int targetX = nextCheckpointX - vx * leadFactor;
-            int targetY = nextCheckpointY - vy * leadFactor;
-
-            // Optional gentle clamp to map bounds to avoid extreme targets
-            targetX = Clamp(targetX, -1000, 17000); // a little leeway is fine
-            targetY = Clamp(targetY, -1000, 10000);
-
-            // Thrust control
-            int thrust = 100;
-            int absAngle = Math.Abs(nextCheckpointAngle);
-
-            // Hard braking if we're pointing away
-            if (absAngle > 90) thrust = 0;
-            else if (absAngle > 70) thrust = 20;
-
-            // Distance-based braking: closer checkpoints -> ease off
-            // Brake distance scales with speed (heuristic)
-            int brakeDist = (int)Math.Max(1200, speed * 6.0);
-            if (nextCheckpointDist < brakeDist)
-            {
-                // Blend: more braking if angle is still significant
-                if (absAngle > 45) thrust = Math.Min(thrust, 30);
-                else thrust = Math.Min(thrust, 55);
+                var s = Console.ReadLine().Split(' ');
+                opp[i].Pos = new Point(int.Parse(s[0]), int.Parse(s[1]));
+                opp[i].V = new Vec(int.Parse(s[2]), int.Parse(s[3]));
+                opp[i].AngleAbs = int.Parse(s[4]);
+                opp[i].NextId = int.Parse(s[5]);
+                opp[i].UpdateProgress(CP_COUNT);
             }
 
-            // BOOST on a perfect long straight once
-            string action = thrust.ToString();
-            if (!boostUsed && absAngle < 5 && nextCheckpointDist > 6000 && speed > 250)
+            // Decide/lock roles
+            DecideRunner();
+
+            // Build orders for both pods, preserving input order
+            var orders = new string[2];
+            for (int i = 0; i < 2; i++)
             {
-                action = "BOOST";
-                boostUsed = true;
-            }
-            else
-            {
-                // SHIELD if we're about to collide (close + high relative speed)
-                if (oppDist < 800 && relV > 300 && speed > 200 && absAngle < 45)
-                {
-                    action = "SHIELD";
-                }
-                else
-                {
-                    action = thrust.ToString();
-                }
+                if (i == runnerId) orders[i] = ControlRunner(my[i], my[1 - i]);
+                else orders[i] = ControlCamper(my[i], my[1 - i]);
             }
 
-            Console.WriteLine($"{targetX} {targetY} {action}");
-
-            // Update state
-            prevX = x; prevY = y;
-            prevOppX = opponentX; prevOppY = opponentY;
+            // Output
+            Console.WriteLine(orders[0]);
+            Console.WriteLine(orders[1]);
         }
     }
 
-    static double Hypot(int dx, int dy)
+    // ================== Runner control ==================
+
+    static string ControlRunner(PodTracker me, PodTracker mate)
     {
-        return Math.Sqrt((double)dx * dx + (double)dy * dy);
+        var cp = CP[me.NextId];
+        var next = CP[(me.NextId + 1) % CP_COUNT];
+
+        // Racing target: aim beyond checkpoint toward the next one, with velocity compensation
+        Vec toNext = (next - cp).Norm();
+        int turnOut = Clamp((int)(me.Speed() * 0.6) + 350, 300, 1400);
+        int leadFactor = 1;
+        Point aim = (cp + toNext * turnOut) - (me.V * leadFactor);
+
+        // If already within the checkpoint circle, push through the exit line
+        double distToCp = (me.Pos - cp).Len();
+        if (distToCp < 650)
+        {
+            int exitPush = 800;
+            aim = cp + toNext * exitPush;
+        }
+
+        // Thrust control based on relative angle
+        double angleToAim = RelativeAngle(me, aim);
+        int absA = Abs(angleToAim);
+        int thrust = 100;
+        if (absA > 100) thrust = 0;
+        else if (absA > 70) thrust = 25;
+
+        // Distance-aware braking (scale with speed)
+        int brakeDist = (int)Math.Max(1200, me.Speed() * 5.5);
+        if (distToCp < brakeDist)
+        {
+            if (absA > 45) thrust = Math.Min(thrust, 30);
+            else thrust = Math.Min(thrust, 60);
+        }
+
+        // BOOST once on a straight, long segment
+        string action = thrust.ToString(CultureInfo.InvariantCulture);
+        if (boostAvailable && absA < 10 && distToCp > 5500 && me.Speed() > 250)
+        {
+            double turnAfter = AngleBetween(cp, next, CP[(me.NextId + 2) % CP_COUNT]);
+            if (Abs(turnAfter) < 25)
+            {
+                action = "BOOST";
+                boostAvailable = false;
+            }
+        }
+
+        // Shield if an opponent is about to ram
+        if (ShouldShield(me, NearestOpponent(me)))
+            action = "SHIELD";
+
+        return $"{aim.X} {aim.Y} {action}";
     }
 
-    static int Clamp(int v, int lo, int hi)
+    // ================== Camper control (checkpoint blocker) ==================
+
+    static string ControlCamper(PodTracker me, PodTracker mate)
     {
-        if (v < lo) return lo;
-        if (v > hi) return hi;
-        return v;
+        // Pick the leading opponent and camp their next checkpoint
+        int t = (opp[0].TotalPassed > opp[1].TotalPassed) ? 0
+              : (opp[0].TotalPassed < opp[1].TotalPassed) ? 1
+              : (opp[0].DistToNext(CP) <= opp[1].DistToNext(CP) ? 0 : 1);
+
+        var targetOpp = opp[t];
+        Point cp = CP[targetOpp.NextId];
+
+        // Inbound direction: where the opponent is coming from
+        Vec inbound = (cp - targetOpp.Pos).Norm();
+        if (inbound.Len() < 1e-6) inbound = (cp - me.Pos).Norm(); // fallback
+
+        // Guard geometry: just outside the cp radius, plus a slight lateral offset to cover the entry arc
+        int guardRad = 780; // place 780 units from center, outside 600 radius
+        int lateral = 180;  // side-step to force a collision or detour
+        Vec perp = new Vec(-inbound.Y, inbound.X).Norm();
+        // Choose lateral side based on which keeps us between opp and cp
+        double side = Math.Sign(((me.Pos - cp).X * perp.X + (me.Pos - cp).Y * perp.Y));
+        if (side == 0) side = 1;
+        Point guardPoint = cp - inbound * guardRad + perp * (lateral * side);
+
+        // If the opponent changed checkpoint or we're far off, beeline to guardPoint
+        double dGuard = (me.Pos - guardPoint).Len();
+
+        // Thrust: enough to hold position once arrived, stronger when repositioning
+        double angleToGuard = RelativeAngle(me, guardPoint);
+        int absA = Abs(angleToGuard);
+        int thrust;
+        if (dGuard > 2500) thrust = absA > 90 ? 50 : 100;
+        else if (dGuard > 900) thrust = absA > 90 ? 40 : 85;
+        else if (dGuard > 400) thrust = absA > 90 ? 30 : 65;
+        else thrust = 35; // hover near spot to keep blocking arc
+
+        string action = thrust.ToString(CultureInfo.InvariantCulture);
+
+        // If an immediate collision is likely, shield to anchor the block
+        if (ShouldShield(me, targetOpp))
+            action = "SHIELD";
+
+        // Avoid friendly hits: if our runner is coming through, ease off
+        var runner = my[runnerId];
+        if ((runner.Pos - me.Pos).Len() < 1100 && runner.TotalPassed >= me.TotalPassed)
+            action = Math.Min(thrust, 40).ToString(CultureInfo.InvariantCulture);
+
+        return $"{guardPoint.X} {guardPoint.Y} {action}";
+    }
+
+    // ================== Utilities & heuristics ==================
+
+    static void DecideRunner()
+    {
+        // Stable role with hysteresis: prefer the one ahead; switch only on clear advantage
+        int suggested = CompareProgress(my[0], my[1]) >= 0 ? 0 : 1;
+        if (runnerId == -1) { runnerId = suggested; return; }
+
+        int current = runnerId, other = 1 - runnerId;
+        int diff = my[other].TotalPassed - my[current].TotalPassed;
+        if (diff >= 2 || (diff == 1 && my[other].DistToNext(CP) + 600 < my[current].DistToNext(CP)))
+            runnerId = other;
+    }
+
+    static int CompareProgress(PodTracker A, PodTracker B)
+    {
+        if (A.TotalPassed != B.TotalPassed) return A.TotalPassed > B.TotalPassed ? 1 : -1;
+        double dA = A.DistToNext(CP), dB = B.DistToNext(CP);
+        if (Math.Abs(dA - dB) > 1e-6) return dA < dB ? 1 : -1;
+        return 0;
+    }
+
+    static PodTracker NearestOpponent(PodTracker me)
+    {
+        var o0 = (opp[0].Pos - me.Pos).Len();
+        var o1 = (opp[1].Pos - me.Pos).Len();
+        return o0 <= o1 ? opp[0] : opp[1];
+    }
+
+    static bool ShouldShield(PodTracker me, PodTracker threat)
+    {
+        Vec relV = me.V - threat.V;
+        Vec relP = threat.Pos - me.Pos;
+        double dist = relP.Len();
+        double closing = relV.X * relP.X + relV.Y * relP.Y; // dot < 0 means approaching
+        return dist < 800 && relV.Len() > 400 && closing < 0;
+    }
+
+    static double RelativeAngle(PodTracker me, Point target)
+    {
+        Vec to = (target - me.Pos).Norm();
+        double angleToTarget = Math.Atan2(to.Y, to.X) * 180.0 / Math.PI; // 0 East, +90 South
+        double rel = NormalizeAngle(angleToTarget - me.AngleAbs);
+        return rel;
+    }
+
+    static double AngleBetween(Point a, Point b, Point c)
+    {
+        Vec v1 = (a - b).Norm();
+        Vec v2 = (c - b).Norm();
+        double ang1 = Math.Atan2(v1.Y, v1.X) * 180.0 / Math.PI;
+        double ang2 = Math.Atan2(v2.Y, v2.X) * 180.0 / Math.PI;
+        return NormalizeAngle(ang2 - ang1);
+    }
+
+    static double NormalizeAngle(double deg)
+    {
+        while (deg <= -180) deg += 360;
+        while (deg > 180) deg -= 360;
+        return deg;
+    }
+
+    static int Clamp(int v, int lo, int hi) => v < lo ? lo : (v > hi ? hi : v);
+    static int Abs(double d) => (int)Math.Abs(d);
+
+    // ================== Types ==================
+
+    struct Point
+    {
+        public int X, Y;
+        public Point(int x, int y) { X = x; Y = y; }
+        public static Vec operator -(Point a, Point b) => new Vec(a.X - b.X, a.Y - b.Y);
+        public static Point operator +(Point a, Vec v) => new Point(a.X + (int)v.X, a.Y + (int)v.Y);
+        public static Point operator -(Point a, Vec v) => new Point(a.X - (int)v.X, a.Y - (int)v.Y);
+        public override string ToString() => $"{X} {Y}";
+        public double Len() => Math.Sqrt((double)X * X + (double)Y * Y);
+    }
+
+    struct Vec
+    {
+        public double X, Y;
+        public Vec(double x, double y) { X = x; Y = y; }
+        public static Vec operator +(Vec a, Vec b) => new Vec(a.X + b.X, a.Y + b.Y);
+        public static Vec operator -(Vec a, Vec b) => new Vec(a.X - b.X, a.Y - b.Y);
+        public static Vec operator *(Vec a, int k) => new Vec(a.X * k, 0);
+        public static Vec operator *(Vec a, double k) => new Vec(a.X * k, a.Y * k);
+        public double Len() => Math.Sqrt(X * X + Y * Y);
+        public Vec Norm()
+        {
+            double l = Len();
+            if (l < 1e-9) return new Vec(0, 0);
+            return new Vec(X / l, Y / l);
+        }
+    }
+
+    class PodTracker
+    {
+        public Point Pos;
+        public Vec V;
+        public int AngleAbs;
+        public int NextId;
+        public int PrevNextId = -1;
+
+        // Monotonic progress as number of checkpoints passed
+        public int TotalPassed = 0;
+
+        public void UpdateProgress(int N)
+        {
+            if (PrevNextId == -1) { PrevNextId = NextId; return; }
+            if (NextId != PrevNextId)
+            {
+                int expected = (PrevNextId + 1) % N;
+                if (NextId == expected) TotalPassed++;
+                else
+                {
+                    int delta = (NextId - PrevNextId + N) % N;
+                    if (delta > 0) TotalPassed += delta;
+                }
+                PrevNextId = NextId;
+            }
+        }
+
+        public int Speed() => (int)new Vec(V.X, V.Y).Len();
+        public double DistToNext(Point[] cps) => (Pos - cps[NextId]).Len();
     }
 }
